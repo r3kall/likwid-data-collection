f406e520aff3e185e624806c4521eb47c70fcf9c
r3kall <lnz.rutigliano@gmail.com>
2020-01-17 19:25:55 +0100

    test change

--- likwid-test.sh
+++ likwid-test.sh
@@ -2,8 +2,8 @@
 source likwid-common.sh
 
 #### GLOBALS #############################################################################
-readonly DIR_AGGREGATED="data/raw"  # mkdir -p dirpath
-readonly WORKLOAD_ARRAY=(copy ddot daxpy_mem_avx daxpy_sp sum triad triad_sp triad_avx stream stream_avx stream_mem_sse update)
+readonly DIR="data/raw"  # mkdir -p dirpath
+readonly WORKLOAD_ARRAY=(copy copy_avx copy_mem daxpy daxpy_avx daxpy_mem_avx daxpy_mem_avx_fma ddot ddot_avx update sum stream stream_avx stream_mem stream_mem_avx triad triad_avx triad_avx_fma triad_mem_avx triad_mem_avx_fma)
 readonly FREQUENCY_ARRAY=(0.8 1.0 1.3 1.5 1.8 2.0 2.2 2.5 2.7 2.9 3.0 3.2)
 readonly NUM_THREAD=(1 2 3 4)
 
@@ -12,7 +12,7 @@ readonly NUM_THREAD=(1 2 3 4)
 # Run a single test.
 # Globals:
 #   LIKWID
-#   DIR_AGGREGATED
+#   DIR
 # Arguments:
 #   $1: (str) test name [ref: likwid-bench -a]
 #   $2: (int) number of cores
@@ -24,7 +24,7 @@ readonly NUM_THREAD=(1 2 3 4)
 #######################################
 function run_test {
   local CS=$(( $2 - 1))
-  $LIKWID -T 100ms -o $DIR_AGGREGATED/$1-$2-$3-$4.csv -C 0-$CS likwid-bench -t $1 -w S0:$4 -i $5 > "$DIR_AGGREGATED/$1-$2-$3-$4.stdout" 2> /dev/null
+  $LIKWID -T 100ms -o $DIR/$1-$2-$3-$4.csv -C 0-$CS likwid-bench -t $1 -w S0:$4 -i $5 > "$DIR/$1-$2-$3-$4.stdout" 2> /dev/null
 }
 
 
@@ -62,7 +62,22 @@ function testing {
 
         echo -e "$(MSG) Test (1GB): $t frequency: $f GHz num_thread: $c"
         t_start=`date +%s`
-        # run_test $t $c $f 1GB 512
+        # run_test $t $c $f 1GB 768
+        echo -e "$(MSG) Test Duration: $(($(date +%s)-$t_start)) seconds"
+        
+        echo -e "$(MSG) Test (128MB): $t frequency: $f GHz num_thread: $c"
+        t_start=`date +%s`
+        # run_test $t $c $f 128MB 6144
+        echo -e "$(MSG) Test Duration: $(($(date +%s)-$t_start)) seconds"
+        
+        echo -e "$(MSG) Test (1MB): $t frequency: $f GHz num_thread: $c"
+        t_start=`date +%s`
+        # run_test $t $c $f 1MB 150000
+        echo -e "$(MSG) Test Duration: $(($(date +%s)-$t_start)) seconds"
+        
+        echo -e "$(MSG) Test (64kB): $t frequency: $f GHz num_thread: $c"
+        t_start=`date +%s`
+        # run_test $t $c $f 64kB 600000
         echo -e "$(MSG) Test Duration: $(($(date +%s)-$t_start)) seconds"
 
       done
@@ -95,9 +110,9 @@ function control_c {
 
 function main() {
   [[ $EUID -ne 0 ]] && echo "This script must be run as root." && exit 1
-  [ ! -d $DIR_AGGREGATED ] && sudo -u $LIKWID_USER mkdir -p $DIR_AGGREGATED
+  [ ! -d $DIR ] && sudo -u $LIKWID_USER mkdir -p $DIR
   testing
-  chown -R $LIKWID_USER:$LIKWID_USER $DIR_AGGREGATED
+  chown -R $LIKWID_USER:$LIKWID_USER $DIR
 }
 
 trap control_c SIGINT
